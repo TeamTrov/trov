cd0414c682ce7f5be60c0ad3b9b68f15be93650d
Jake Holtz <jahholtz@gmail.com>
2017-05-03 15:10:16 -0700

    Modified db.js, server.js, app.js for functionality

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require("express");
+var db - require('./db');
+
+// middleware
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+
+// misc
 var favicon = require('favicon'); //use this to add icon to webpage
 
+// routing
 var app = express();
+
+// ports
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Listening on port " + port);
+
+// logging and parsing
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-
-var port = process.env.PORT || 3000; //
-app.listen(port);
-
-console.log("Listening on port " + port);
-
+// serving client files
 app.use(express.static('src/client'));
--- src/server/db.js
+++ src/server/db.js
@@ -1,2 +1,19 @@
 var mysql = require('mysql');
 var sequelize = require('sequelize');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    database: 'trov'
+});
+
+connection.connect((err) => {
+  if(err) {
+    console.error(error)
+  } else {
+    console.log('DATABASE connection established');
+  }
+});
+
+module.exports.connection = connection;
