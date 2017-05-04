ff5b85e70b624d00a4556455a69aacaef193edbf
Jake Holtz <jahholtz@gmail.com>
2017-05-03 20:05:45 -0700

    Connected server.js to db.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var server = express.Router();
+var db = require('./db.js')
 
 // routing requests from app.js
 server.use(function(req, res, next) {
@@ -7,7 +8,7 @@ server.use(function(req, res, next) {
   next();
 });
 
-// express get route commented out for now
+// express get route commented out for now:
 
 // server.get('/', function(req, res) {
 //   console.log('home page')
