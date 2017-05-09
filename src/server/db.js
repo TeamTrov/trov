5dc0df7b5cd3b8e0860b0424dc3549b11a2c53ff
Jake Holtz <jahholtz@gmail.com>
2017-05-09 16:18:50 -0700

    Database (db.js) all functional and connected

--- src/server/db.js
+++ src/server/db.js
@@ -1,2 +1,35 @@
+var express    = require("express");
 var mysql = require('mysql');
-var sequelize = require('sequelize');
+var app = express();
+
+
+// establish database connection
+var connection = mysql.createConnection({
+  port     : 3306,
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+});
+
+
+// handle database connection errors
+connection.connect(function(err){
+  if(!err) {
+      console.log("SERVER-DATABASE connected ...");
+  } else {
+      console.log(err);
+  }
+});
+
+function getValueOfKey(table) {
+  connection.query(`SELECT ${key} FROM ${table};`,
+    function(error, success) {
+      if (error) {
+        console.error('ADD TO DB ERROR: ' + error);
+      }
+    }
+  );
+}
+
+
+module.exports.connection = connection;
