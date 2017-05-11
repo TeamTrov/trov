15795402357220aea97214b908198190f07e153c
Jake Holtz <jahholtz@gmail.com>
2017-05-11 13:51:24 -0700

    Added method that queries database for current user's trove

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ server.use(function(req, res, next) {
 });
 
 // creating tables in database
+// db.connection.query(`DROP DATABASE trov;`);
 db.connection.query(`CREATE DATABASE IF NOT EXISTS trov;`);
 db.connection.query(`USE trov;`);
 db.connection.query(`CREATE TABLE IF NOT EXISTS users (
@@ -20,19 +21,18 @@ db.connection.query(`CREATE TABLE IF NOT EXISTS users (
 db.connection.query(`CREATE TABLE IF NOT EXISTS trovs (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) UNIQUE,
-  createdBy INT,
+  createdBy VARCHAR(50),
   numberOfUsers VARCHAR(10),
   currentProgress INT,
   challenges VARCHAR(50)
 );`);
 db.connection.query(`CREATE TABLE IF NOT EXISTS users_trovs (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  userId INT,
-  trovId INT,
-  currentChallenge INT,
-  totalChallenges INT,
-  FOREIGN KEY (userId) REFERENCES users(id),
-  FOREIGN KEY (trovId) REFERENCES trovs(id)
+  userId VARCHAR(50),
+  trovId VARCHAR(50),
+  isCurrentTrov BOOL DEFAULT false,
+  currentChallengeNo INT,
+  totalChallengesNo INT
 );`);
 db.connection.query(`CREATE TABLE IF NOT EXISTS challenges (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -46,10 +46,12 @@ db.connection.query(`CREATE TABLE IF NOT EXISTS challenges (
 
 // test data
 // db.connection.query(`USE trov;`);
-// db.connection.query(`INSERT INTO users (username, facebookId, email) VALUES ('jholtz', 'jakeholtz', 'jahholtz@gmail.com');`);
-// db.connection.query(`INSERT INTO users (username, facebookId, email) VALUES ('dholtz', 'dakeholtz', 'dahholtz@gmail.com');`);
-// db.connection.query(`INSERT INTO trovs (name, createdBy, numberOfUsers, currentProgress, challenges) VALUES ('Mission Madness', 2, 2, 0, '[1, 2, 3]');`);
-// db.connection.query(`INSERT INTO trovs (name, createdBy, numberOfUsers, currentProgress, challenges) VALUES ('Kalua Kraziness', 1, 1, 0, '[4, 5, 6]');`);
+// db.connection.query(`INSERT INTO users (username, facebookId, email) VALUES ('jholtz', 'jjjholtz', 'jazz@gmail.com');`);
+// db.connection.query(`INSERT INTO users (username, facebookId, email) VALUES ('dholtz', 'dddholtz', 'dazz@gmail.com');`);
+// db.connection.query(`INSERT INTO trovs (name, createdBy, numberOfUsers, currentProgress, challenges) VALUES ('Mission Madness', 'dholtz', 2, 0, '[1, 2, 3]');`);
+// db.connection.query(`INSERT INTO trovs (name, createdBy, numberOfUsers, currentProgress, challenges) VALUES ('Kalua Kraziness', 'jholtz', 1, 0, '[4, 5, 6]');`);
+// db.connection.query(`INSERT INTO users_trovs (userId, trovId, isCurrentTrov, currentChallengeNo, totalChallengesNo) VALUES ('jholtz', 'Mission Madness', false, 10, 10);`);
+// db.connection.query(`INSERT INTO users_trovs (userId, trovId, isCurrentTrov, currentChallengeNo, totalChallengesNo) VALUES ('jholtz', 'Kalua Kraziness', true, 1, 10);`);
 
 // *** GET ALL TROVES FROM DB **
 server.get('/getalltrovs', function(req, res) {
@@ -111,10 +113,24 @@ server.post('/addnewusertodb', function(req, res) {
 });
 
 // *** GET USER'S CURRENT TROVE **
-server.get('/getuserdata', function(req, res) {
-  // input: username
-  // go to database
-  // return current trove of user and progress
+server.get('/getusertrovdata', function(req, res) {
+  var trovData;
+  var username = "jholtz"; // needs to be updated
+  db.connection.query(`use trov`);
+  db.connection.query(`SELECT * FROM users_trovs WHERE userId = "${username}" AND isCurrentTrov = true;`,
+    function(error, result) {
+      if(error) {
+        console.log("Error querying database (/getusertrovdata)");
+      } else {
+        if (result.length !== 0) {
+          trovData = JSON.stringify(result);
+          res.end(trovData);
+        } else {
+          console.log(`User not currently on a trove!`);
+        }
+      }
+    }
+  )
 });
 
 module.exports = server;
